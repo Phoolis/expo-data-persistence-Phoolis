0a665c1e167af5f067443849334109ec4d94e94d
Phoolis <carlsonps@gmail.com>
2025-02-26 17:17:24 +0200

    Add check to onValue for empty database

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -5,54 +5,63 @@ import { onValue, push, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 import { Button, FlatList, Text, TextInput } from "react-native";
 
-type Product = { title: string, amount: string };
+type Product = { title: string; amount: string };
 
 export default function Index() {
-
-  const [product, setProduct] = useState({ title: '', amount: '' });
+  const [product, setProduct] = useState({ title: "", amount: "" });
   const [items, setItems] = useState<Product[]>([]);
 
   const handleSave = () => {
     push(ref(database, "items/"), product);
-  }
+  };
 
   useEffect(() => {
     onValue(ref(database, "items/"), (snapshot) => {
       const data = snapshot.val();
+      if (!data) return;
       const firebaseItems = Object.values(data);
       setItems(firebaseItems as Product[]);
-    })
+    });
   }, []);
 
   return (
     <Screen>
       <Title>Firebase</Title>
       <Paragraph>
-        Firebase is a platform developed by Google that provides a variety of tools and services for building and managing mobile and web applications.
+        Firebase is a platform developed by Google that provides a variety of
+        tools and services for building and managing mobile and web
+        applications.
       </Paragraph>
       <Paragraph>
-        Firebase provides Realtime Database that is NoSQL cloud database. Data is stored as JSON and synchronized in realtime to every connected client.
+        Firebase provides Realtime Database that is NoSQL cloud database. Data
+        is stored as JSON and synchronized in realtime to every connected
+        client.
       </Paragraph>
 
       <TextInput
-        placeholder='Product title'
+        placeholder="Product title"
         style={styles.textInput}
-        onChangeText={text => setProduct({ ...product, title: text })}
-        value={product.title} />
+        onChangeText={(text) => setProduct({ ...product, title: text })}
+        value={product.title}
+      />
 
       <TextInput
-        placeholder='Amount'
+        placeholder="Amount"
         style={styles.textInput}
-        onChangeText={text => setProduct({ ...product, amount: text })}
-        value={product.amount} />
+        onChangeText={(text) => setProduct({ ...product, amount: text })}
+        value={product.amount}
+      />
 
       <Button onPress={handleSave} title="Save" />
 
       <FlatList
-        renderItem={({ item }) =>
-          <Text style={{ fontSize: 18 }}>{item.title}, {item.amount}</Text>
-        }
-        data={items} />
-    </Screen >
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 18 }}>
+            {item.title}, {item.amount}
+          </Text>
+        )}
+        data={items}
+      />
+    </Screen>
   );
 }
--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+  apiKey: "AIzaSyC7R7K3WzWWWIkWm9AT3d0uOZK2YX30Icc",
+  authDomain: "ekaprojekti-cc49e.firebaseapp.com",
+  databaseURL:
+    "https://ekaprojekti-cc49e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ekaprojekti-cc49e",
+  storageBucket: "ekaprojekti-cc49e.firebasestorage.app",
+  messagingSenderId: "971489049084",
+  appId: "1:971489049084:web:b0ac53cdde091320c99a79",
 };
 
 export const app = initializeApp(firebaseConfig);
